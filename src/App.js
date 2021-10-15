f6b8691f3e1789316a08403e716da5c304a4a875
kundanad26 <kundanad2002@gmail.com>
2021-10-15 23:05:43 +0530

    Text changed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>This is Project one</p>
+        <p>This is Git Project</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
